9ce745c115de28406edd50e3a6fa96d2b914037d
kevin <kivito2009@hotmail.com>
2022-04-27 08:17:24 -0500

    creacion de servicio principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NotFoundComponent } from './not-found/not-found.component';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NotFoundComponent} from './not-found/not-found.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundComponent
+    NotFoundComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/core/general/principal.service.ts
+++ src/app/core/general/principal.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import {Inject, Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class PrincipalService <Interface>{
+export class PrincipalService<Interfaz> {
 
-  constructor() { }
+  constructor(
+    protected readonly _httpClient: HttpClient,
+    @Inject('url')
+    private readonly url: string,
+    @Inject('segmento')
+    private readonly segmento: string,
+    @Inject('port')
+    private readonly port: number,
+  ) {
+  }
+
+  findAll(consulta?: string): Observable<[Interfaz[], number]> {
+    let baseUrl = `${this.url}:${this.port}/${this.segmento}`;
+    if (consulta) {
+      baseUrl = `${baseUrl}?${consulta}`;
+    }
+    return this._httpClient.get(baseUrl) as Observable<[Interfaz[], number]>;
+  }
+
+  updateById(id: number, payload: Interfaz): Observable<Interfaz> {
+    return this._httpClient.put(
+      `${this.url}:${this.port}/${this.segmento}/${id}`,
+      payload,
+    ) as Observable<Interfaz>;
+  }
+
+  createOne(payload: Interfaz): Observable<Interfaz> {
+    return this._httpClient.post(
+      `${this.url}:${this.port}/${this.segmento}`,
+      payload,
+    ) as Observable<Interfaz>;
+  }
+
+  createMany(payload: Interfaz): Observable<Interfaz> {
+    return this._httpClient.post(
+      `${this.url}:${this.port}/${this.segmento}`,
+      payload,
+    ) as Observable<Interfaz>;
+  }
+
+  findOneById(id: number): Observable<Interfaz> {
+    return this._httpClient.get(
+      `${this.url}:${this.port}/${this.segmento}/${id}`,
+    ) as Observable<Interfaz>;
+  }
 }
